408977f9dd86241991adc56737660a679663660e
tjhubert <tjhubert@live.com>
2015-10-13 21:43:05 -0500

    object and array tutorial

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 'use strict'
 
-alert("hey");
+var source   = $("#some-template").html();
+var template = Handlebars.compile(source);
 
-$("#firstdiv").on( "click",function(){
-    alert("yoo");
-} );
+var data = {users: [
+  {username: "alan", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
+  {username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
+  {username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
+]};
+
+var newUsernameEntry = ["john", "erica", "xavier", "chloe", "robert"];
+newUsernameEntry.lol = "you shouldn't be printing this";
+
+var newFirstnameEntry = ["John", "Erica", "Xavier", "Chloe", "Robert"];
+newFirstnameEntry.lol = "you shouldn't be printing this";
+
+var newLastnameEntry = ["Lambert", "Lo", "White", "Nguyen", "Peterson"];
+newLastnameEntry.lol = "you shouldn't be printing this";
+
+var newEmailEntry = ["john@test.com", "erica@test.com", "xavier@test.com", "chloe@test.com", "robert@test.com"];
+newEmailEntry.lol = "you shouldn't be printing this";
+
+// TODO: update the object data here with the new entries!
+
+$("#content-placeholder").html(template(data));
+
