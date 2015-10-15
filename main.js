0f8476e5aad7674965c7442a09f0d7d72fb94556
Hertin <hertin@outlook.com>
2015-10-14 21:32:29 -0500

    what

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 'use strict'
 
-alert("hey");
-
+//alert("hey");
+var i=true;
 $("#firstdiv").on( "click",function(){
-    alert("yoo");
+	if(i){
+		//console.log($(this).css.backgroundColor);
+//	if($(this).css.("background-color")==="blue")
+	
+		$(this).css("background-color", "black" );
+		
+	}else
+	{
+		$(this).css("background-color","blue");
+		
+	}
+	i=!i;
+/*	if(i){
+    	document.getElementById("firstdiv").backgroundColor="green";
+    	i=false;
+    }
+    else{
+    	document.getElementById("firstdiv").backgroundColor="black";
+    	i=true;
+    }*/
+    //alert("yoo");
 } );
