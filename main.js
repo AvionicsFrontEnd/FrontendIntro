4e1b6b477868f55466c5af9abad1a19fb8b8bedc
tjhubert <tjhubert@live.com>
2015-10-21 15:59:49 -0500

    yeah part 2

--- main.js
+++ main.js
@@ -7,28 +7,52 @@ var data = {users: [
   {username: "alan", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
   {username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
   {username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
-]};
+],
+startPartTwo: false};
 
 var newUsernameEntry = ["john", "erica", "xavier", "chloe", "robert"];
-newUsernameEntry.lol = "you shouldn't be printing this";
-newUsernameEntry[1000] = "Lolz";
 
 var newFirstnameEntry = ["John", "Erica", "Xavier", "Chloe", "Robert"];
-newFirstnameEntry.lol = "you shouldn't be printing this";
-newFirstnameEntry.length = 666;
 
 var newLastnameEntry = ["Lambert", "Lo", "White", "Nguyen", "Peterson"];
-newLastnameEntry[0] = "Lambert";
-newLastnameEntry[5] = "Lo";
-newLastnameEntry[43] = "White";
-newLastnameEntry[99] = "Nguyen";
-newLastnameEntry[102] = "Peterson";
-newLastnameEntry.lol = "you shouldn't be printing this";
 
 var newEmailEntry = ["john@test.com", "erica@test.com", "xavier@test.com", "chloe@test.com", "robert@test.com"];
-newEmailEntry.lol = "you shouldn't be printing this";
 
+// PART I
 // TODO: update the object data here with the new entries!
+
+
+
+
+// PART II
+
+var newAgeField = ["13", "17", "20", "18", "23", "40", "31", "34"];
+
+var newStateField = ["IL", "MO", "NY", "CA", "CA", "TX", "PO", "IN"];
+
+var startPartTwo = function() {
+    data.startPartTwo = true;
+    var falsy_var;
+    newAgeField.lol = "you shouldn't be printing this";
+    newAgeField[1000] = falsy_var;
+    newStateField = [];
+    newStateField[3] = "IL";
+    newStateField[12] = "MO";
+    newStateField[32] = "NY";
+    newStateField[35] = "CA";
+    newStateField[49] = "CA";
+    newStateField[100] = "TX";
+    newStateField[1023] = "PO";
+    newStateField[9999] = "IN";
+    newStateField.length = 1000000;
+}
+
+// uncomment the code below to start part 2
+// startPartTwo();
+
+//TODO: write your code for part 2 here, introduce new age and state property in the data.users array
+// must use 2 identical loops only to finish this task
+
 
 $("#content-placeholder").html(template(data));
 
