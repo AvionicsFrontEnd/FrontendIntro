dacd781a3b83f5e88dda9d75bdbf4ed5cc96c758
tjhubert <tjhubert@live.com>
2015-10-21 17:29:56 -0500

    finalizing stuff

--- main.js
+++ main.js
@@ -25,26 +25,27 @@ var newEmailEntry = ["john@test.com", "erica@test.com", "xavier@test.com", "chlo
 
 
 // PART II
-
-var newAgeField = ["13", "17", "20", "18", "23", "40", "31", "34"];
-
-var newStateField = ["IL", "MO", "NY", "CA", "CA", "TX", "PO", "IN"];
-
+// MUST DO PART I before starting PART II
+var newAgeField;
+var newStateField = [];
 var startPartTwo = function() {
     data.startPartTwo = true;
+    newAgeField = ["13", "17", "20", "18", "23", "40", "31", "34"];
     var falsy_var;
     newAgeField.lol = "you shouldn't be printing this";
     newAgeField[1000] = falsy_var;
-    newStateField = [];
     newStateField[3] = "IL";
+    newStateField[4] = falsy_var;
     newStateField[12] = "MO";
+    newStateField[20] = falsy_var;
     newStateField[32] = "NY";
     newStateField[35] = "CA";
     newStateField[49] = "CA";
     newStateField[100] = "TX";
     newStateField[1023] = "PO";
+    newStateField[2000] = falsy_var;
     newStateField[9999] = "IN";
-    newStateField.length = 1000000;
+    newStateField.length = 1;
 }
 
 // uncomment the code below to start part 2
@@ -52,6 +53,9 @@ var startPartTwo = function() {
 
 //TODO: write your code for part 2 here, introduce new age and state property in the data.users array
 // must use 2 identical loops only to finish this task
+// check these console logs first before starting
+// console.log(newAgeField);
+// console.log(newStateField);
 
 
 $("#content-placeholder").html(template(data));
